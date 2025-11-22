1fd1dc0ba613aeb79afd571d6cfe811aae39360f
Maryam <mablahatova@gmail.com>
2025-11-22 11:00:01 +0600

    test git flow #1

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>start</h1>
+      <button>test</button>
     </div>
   );
 }
