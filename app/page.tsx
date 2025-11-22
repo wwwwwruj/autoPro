bbcc3f1ca0f2c765f65956ba7bec7a852d90ae42
Maryam <mablahatova@gmail.com>
2025-11-22 12:01:39 +0600

    test-2 #4

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>start</h1>
-      <button>test</button>
+      <button>test</button> 
+      <button>Войти</button>
+      <button>Подключить</button>
     </div>
+   
   );
 }
